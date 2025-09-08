a57ad18195be483e7ee28efc8808ee3c7204ef87
omer1998 <omerfaris11@gmail.com>
2025-09-08 14:41:50 +0300

    update entity to have image url

--- src/main/kotlin/com/spring_test/demo/model/Contact.kt
+++ src/main/kotlin/com/spring_test/demo/model/Contact.kt
@@ -5,10 +5,10 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-
 @Entity(name = "contacts")
-class Contact(
+data class Contact(
     @Id @GeneratedValue(strategy = GenerationType.AUTO) val id: Long? = null,
-    @Column(name = "contact_name") val contactName: String,
-    @Column(name = "phone_number") val phoneNumber: String
+    @Column(name = "contact_name", unique = true) val contactName: String,
+    @Column(name = "phone_number") val phoneNumber: String,
+    @Column(name= "img_url") val imageUrl : String = ""
 )
